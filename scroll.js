24b3d77b12db82433a3eae3a6000f12b4372b05e
Prerit Oberai <preritoberai@yahoo.com>
2015-04-17 09:53:19 -0600

    scroll

--- scroll.js
+++ scroll.js
@@ -1,4 +1,6 @@
- /*$('a[href*=#Home]:not([href=#])').click(function() {
+$(document).ready(function(){
+
+$('a[href*=#Home]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
@@ -10,10 +12,31 @@
       }
     }
   });
-  */
-  $("#button1").click(function() {
-    $('html, body').animate({
-        scrollTop: $("#Home").offset().top
-    }, 2000);
-});
 
+$('a[href*=#Info]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+  
+  $('a[href*=#Download]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+
+});
